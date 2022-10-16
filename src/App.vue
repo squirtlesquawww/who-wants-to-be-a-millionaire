34a703c600811d9675bf5accc8d0edaa954adc63
Jane <jerrikanecode@gmail.com>
2022-10-16 14:01:24 -0700

    Add tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ 
+  <WWTBAMWrapper />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WWTBAMWrapper from './components/WWTBAMWrapper.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    WWTBAMWrapper
   }
 }
 </script>
---
+++ src/components/WWTBAMWrapper.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="text-3xl">
+    question
+    A 
+    B 
+    C 
+    D 
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WWTBAMWrapper',
+}
+</script>
