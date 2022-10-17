4508123323c0b8093e4cf1889bb3c7b3f711e947
Jane <jerrikanecode@gmail.com>
2022-10-16 23:22:29 -0700

    put questions in different places

---
+++ src/components/QuestionBlock.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="text-3xl">
+        {{ question }}
+
+        <div class="grid grid-cols-2 gap-4">
+            <div>A. {{ answerA }}</div>
+            <div>B. {{ answerB }}</div>
+            <div>C. {{ answerC }}</div>
+            <div>D. {{ answerD }}</div>
+        </div>
+
+        <!-- <input type="text" v-model="question2"> -->
+        <!-- {{ question2 }} -->
+    </div>
+</template>
+  
+<script>
+
+export default {
+    name: 'QuestionBlock',
+    props: {
+        question: { type: String, required: true },
+        answerA: { type: String, required: true },
+        answerB: { type: String, required: true },
+        answerC: { type: String, required: true },
+        answerD: { type: String, required: true },
+    },
+    data: () => {
+        return {
+            question2: 'slafdjskdlf'
+        }
+    }
+}
+</script>
+  
--- src/components/WWTBAMWrapper.vue
+++ src/components/WWTBAMWrapper.vue
@@ -1,15 +1,48 @@
 <template>
-  <div class="text-3xl">
-    question
-    A 
-    B 
-    C 
-    D 
+  <div>
+    <QuestionBlock question="who the top scorer in NBA?" answerA="kobe" answerB="jerrik" answerC="kareem"
+      answerD="magic" />
+
+    <div v-for="(question, index) in questions" :key="index">
+      <QuestionBlock :question="question.question" :answerA="question.answerA" :answerB="question.answerB"
+        :answerC="question.answerC" :answerD="question.answerD" />
+
+    </div>
   </div>
 </template>
 
 <script>
+import QuestionBlock from './QuestionBlock.vue';
+
 export default {
-  name: 'WWTBAMWrapper',
+  name: "WWTBAMWrapper",
+  components: { QuestionBlock },
+  data: () => {
+    return {
+      questions: [
+        {
+          question: 'Question 1',
+          answerA: 'Answer A',
+          answerB: 'Answer B',
+          answerC: 'Answer C',
+          answerD: 'Answer D'
+        },
+        {
+          question: 'Question 2',
+          answerA: 'Answer A',
+          answerB: 'Answer B',
+          answerC: 'Answer C',
+          answerD: 'Answer D'
+        },
+        {
+          question: 'Question 3',
+          answerA: 'Answer A',
+          answerB: 'Answer B',
+          answerC: 'Answer C',
+          answerD: 'Answer D'
+        }
+      ]
+    }
+  }
 }
 </script>
