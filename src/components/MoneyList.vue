be5cfb24fe7e959d606af7e77b439fd839bdabce
Jane <jerrikanecode@gmail.com>
2022-10-18 22:07:38 -0700

    added MoneyList component

---
+++ src/components/MoneyList.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="text-3xl border-2 border-solid border-slate-500 bg-black text-white text-right">
+        <!-- <li v-for="item in items" :key="item.message"> -->
+        <!-- {{ item.message }} -->
+        <!-- </li> -->
+        <div class="grid grid-cols-3" v-for="(amount, index) in reversedAmounts()" :key="amount">
+            <div></div>
+            <div></div>
+            <div class="text-3xl border-2 border-solid border-slate-500 bg-black text-white text-right"
+                :class="{ 'bg-yellow-600': activeAmount === index }">
+                {{ amounts.length-index }} ${{ amount }}
+            </div>
+        </div>
+    </div>
+</template>
+             
+<script>
+export default {
+    name: 'MoneyList',
+    props: {
+        activeAmount: { type: Number, required: false, default: 0 },
+    },
+    data: () => {
+        return {
+            amounts: [
+                100,
+                200,
+                300,
+                500,
+                1000,
+                2000,
+                4000,
+                8000,
+                16000,
+                32000,
+                64000,
+                125000,
+                250000,
+                500000,
+                1000000
+            ],
+            // items: [{ message: 'Foo' }, { message: 'Bar' }]
+        }
+    },
+    methods: {
+        reversedAmounts() {
+            let reversedArray = [...this.amounts].reverse();
+            return reversedArray;
+            // return this.amounts.reverse();
+
+        }
+    },
+}
+</script>
+             
--- src/components/WWTBAMWrapper.vue
+++ src/components/WWTBAMWrapper.vue
@@ -3,16 +3,18 @@
     <MoneyBox money="1" />
     <QuestionBlock question="who the top scorer in NBA?" answerA="kobe" answerB="jerrik" answerC="kareem"
       answerD="magic" />
+    <MoneyList />
   </div>
 </template>
 
 <script>
 import QuestionBlock from './QuestionBlock.vue';
 import MoneyBox from './MoneyBox.vue';
+import MoneyList from './MoneyList.vue';
 
 export default {
   name: "WWTBAMWrapper",
-  components: { QuestionBlock, MoneyBox },
+  components: { QuestionBlock, MoneyBox, MoneyList },
   data: () => {
     return {
       questions: [
