10e6fed8968ffeb014da30803120b4a671d9005f
Jerrik <jan@pop-os.localdomain>
2022-10-24 23:30:10 -0700

    actual buttons with placeholder icons

--- src/components/LifeLineButtons.vue
+++ src/components/LifeLineButtons.vue
@@ -1,15 +1,35 @@
 <template>
     <div class="text-3xl border-2 border-solid border-slate-500 bg-black text-white text-right">
-        <button class="button">50:50</button>
-        <button>Ask the Audience</button>
-        <button>Phone a friend</button>
+        <button
+            class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+            <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
+            </svg>
+            <span>50/50</span>
+        </button>
+
+        <button
+            class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+            <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
+            </svg>
+            <span>Ask the Audience</span>
+        </button>
+
+        <button
+            class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+            <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
+            </svg>
+            <span>Phone a friend</span>
+        </button>
     </div>
 </template>
   
 <script>
 export default {
     name: 'LifeLineButtons',
-   
+
 }
 </script>
   
